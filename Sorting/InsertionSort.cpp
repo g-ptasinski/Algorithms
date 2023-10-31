aefaf7cc7ed57a2479d1128e700fcfb44a285b34
grzesioo66 <grzesioo66@gmail.com>
2023-10-31 22:13:13 +0100

    Added insertion sort

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <iostream>
 #include <array>
+#include <algorithm>
  
 
-//O(n) requires only one traversal
-void InsertionSort(std::array<int,10>& A)
+//O(n^2) 
+void InsertionSort(std::array<int,10>& A, int n)
 {
-    for(auto& i : A)
+    for(int i = 1; i<n; i++)
     {
+        int j = i;
 
-        std::cout<< i <<std::endl;
+        while(A[j] < A[j-1] && j>0)
+        {
+            std::swap(A[j], A[j-1]);
+
+            j = j-1;
+        }
+
     }
 }
 
 
 int main()
 {
-    std::array<int,10> A = { 0, 1, 23, 22, 61, 120, 410, 278, -340, -41};
+    std::array<int,10> A = { 1, 0, 23, 22, 61, 120, 410, 278, -340, -41};
     int n = sizeof(A)/sizeof(A[0]);
 
     for(auto& i : A)
     {
         std::cout<< i <<  " , " ;
     }
- 
-    InsertionSort(A);
+    std::cout<<std::endl;
+    InsertionSort(A , n);
 
+
+    std::cout<<std::endl<<"Post sorting: "<<std::endl;
     for(auto& i : A)
     {
         std::cout<< i <<  " , " ;
     }
 
-    std::cout<<"/n";
+    std::cout<<std::endl;
 
     return 0;
 }
