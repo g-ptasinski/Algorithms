4f147e413c9389d29251c649b1ec39d5017ec127
grzesioo66 <grzesioo66@gmail.com>
2023-11-01 21:51:46 +0100

    Added bubble sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <iostream>
+#include <array>
+#include <algorithm>
+ 
+
+//O(n^2), in-place, stable
+void BubbleSort(std::array<int,10>& A, int n)
+{
+    int counter = 1;
+
+    while(counter)
+    { 
+        counter = 0;
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(A[i+1]<A[i])
+            {
+             std::swap(A[i], A[i+1]);
+             counter++;   
+            }
+            
+        }
+    }
+}
+
+void BubbleSortRecursive(std::array<int,10>& A, int n, int start_index)
+{
+}
+
+
+
+int main()
+{
+    std::array<int,10> A = { 1, 0, 23, 22, 61, 120, 410, 278, -340, -41};
+    int n = sizeof(A)/sizeof(A[0]);
+
+    for(auto& i : A)
+    {
+        std::cout<< i <<  " " ;
+    }
+    std::cout<<std::endl;
+    BubbleSort(A , n);
+
+
+    std::cout<<std::endl<<"Post sorting: "<<std::endl;
+    for(auto& i : A)
+    {
+        std::cout<< i <<  " " ;
+    }
+
+    std::cout<<std::endl;
+
+    return 0;
+}
