4c3602b1f79fd7af598b2ac9d005c284d7d86d9f
ptasinski.grzegorz <39106050+g-ptasinski@users.noreply.github.com>
2025-01-09 20:33:57 +0100

    Create 1485.CheckIfaWorddOccursAsaPrefixofAnyWordinaSentence.cpp

---
+++ Leetcode/1401-1500/1485.CheckIfaWorddOccursAsaPrefixofAnyWordinaSentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        
+        if(sentence.find(searchWord) != string::npos)
+        {
+            int pos = sentence.find(searchWord);
+            int myCount = 1 + std::count(sentence.begin(), sentence.begin()+pos, ' ');
+            return myCount;        
+        }
+        else
+        {
+            return -1;
+        }
+
+        return -1;        
+    }
+};
