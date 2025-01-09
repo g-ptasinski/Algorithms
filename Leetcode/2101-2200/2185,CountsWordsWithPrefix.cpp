a6abd365bef2b852db3444a066dd2dfa25efd123
ptasinski.grzegorz <39106050+g-ptasinski@users.noreply.github.com>
2025-01-09 20:19:19 +0100

    Create 2185,CountsWordsWithPrefix.cpp

---
+++ Leetcode/2101-2200/2185,CountsWordsWithPrefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+
+        for(auto word : words)
+        {
+            if( word.find(pref) == 0 )
+            {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
