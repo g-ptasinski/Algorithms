ec902de736a09f8b743f6043b513e51ca6d6da02
ptasinski.grzegorz <39106050+g-ptasinski@users.noreply.github.com>
2025-04-02 22:46:14 +0200

    Create 2873.maximumTripletValue.cpp

---
+++ Leetcode/2801-2900/2873.maximumTripletValue.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int maxElem = nums[0];
+
+        vector<int> diffs = nums;
+        vector<int> maxright = nums;
+        
+        for(auto& item : diffs) // calculate max element to the left, and then try to get the difference
+        {
+            maxElem = max(maxElem, item);
+            //if max element is bigger than current difference is 0
+            int tmp = maxElem - item; 
+            if(tmp < 0){tmp = 0;}
+            item = tmp;
+        }
+
+        diffs[0] = 0;
+
+        int maxRight = nums[nums.size()-1];
+
+        for(int i = nums.size()-2; i>=0; --i)
+        {
+            maxRight = max(maxRight, nums[i+1]);
+            maxright[i] = maxRight;
+        }
+        maxright[maxright.size()-1]=0;
+
+        long long maxVal = 0;
+
+        for(int i = 1; i<maxright.size(); ++i)
+        {
+            maxVal = max(maxVal, (long long)maxright[i]*diffs[i]);
+        }
+
+        return maxVal;
+    }
+};
