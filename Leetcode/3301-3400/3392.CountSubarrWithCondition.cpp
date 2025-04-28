c9ff14a1c42ee66b20409a81f887c33ac3351f14
ptasinski.grzegorz <39106050+g-ptasinski@users.noreply.github.com>
2025-04-28 22:18:55 +0200

    Create 3392.CountSubarrWithCondition.cpp

---
+++ Leetcode/3301-3400/3392.CountSubarrWithCondition.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int counter = 0;
+        int left = 0;
+        int mid = 1;
+        int right = 2;
+
+        while(left < nums.size()-2)
+        {
+            mid = left +1;
+            right = mid +1;
+
+            if(2*(nums[left]+nums[right]) == nums[mid])
+            {
+                ++counter;
+            }
+
+            ++left;
+        }
+
+        return counter;
+    }
+};
